5356b17d50eccea665082b6323bda17ca605226c
PedroAyon <pedroayonb@gmail.com>
2025-02-27 08:27:16 -0600

    dp solution fix?

--- dp_solver.py
+++ dp_solver.py
@@ -1,26 +1,34 @@
 # dp_solver.py
 
-def solve(values, weights, capacity):
-    n = len(values)
-    dp = [0] * (capacity + 1)
-    # Array para registrar el último ítem agregado en cada capacidad
-    choice = [-1] * (capacity + 1)
+def solve(weights, values, capacity):
+    # Making the dp array
+    n = len(weights)
+    dp = [0 for i in range(capacity + 1)]
 
-    for i in range(n):
-        for w in range(capacity, weights[i] - 1, -1):
-            if dp[w - weights[i]] + values[i] > dp[w]:
-                dp[w] = dp[w - weights[i]] + values[i]
-                choice[w] = i
+    # Taking first i elements
+    for i in range(1, n + 1):
 
-    # Reconstrucción de la solución
-    solution = [0] * n
-    w = capacity
-    while w > 0 and choice[w] != -1:
-        i = choice[w]
-        # Si el ítem aún no ha sido agregado, lo marcamos
-        if solution[i] == 0:
-            solution[i] = 1
-            w -= weights[i]
-        else:
-            break  # En caso poco probable de duplicación
-    return solution
+        # Starting from back,
+        # so that we also have data of
+        # previous computation when taking i-1 items
+        for w in range(capacity, 0, -1):
+            if weights[i - 1] <= w:
+                # Finding the maximum value
+                dp[w] = max(dp[w], dp[w - weights[i - 1]] + values[i - 1])
+
+    return dp[capacity]
+
+def solve_2d(W, wt, val):
+    n = len(wt)
+    dp = [[0 for _ in range(W + 1)] for _ in range(n + 1)]
+
+    # Build table dp[][] in bottom-up manner
+    for i in range(n + 1):
+        for w in range(W + 1):
+            if i == 0 or w == 0:
+                dp[i][w] = 0
+            elif wt[i - 1] <= w:
+                dp[i][w] = max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w])
+            else:
+                dp[i][w] = dp[i - 1][w]
+    return dp[n][W]
